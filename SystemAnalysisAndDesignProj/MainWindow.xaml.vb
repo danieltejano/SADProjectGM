5f8fdde292893b900cca4f2c2996027497c981b7
User <User@PC2018051215MZP>
2019-11-02 14:39:43 +0800

    Revert "dafault account implemented"
    This reverts commit 46780bc7434bb216f3dc824f8b0dc43a5818c7fe.

--- SystemAnalysisAndDesignProj/MainWindow.xaml.vb
+++ SystemAnalysisAndDesignProj/MainWindow.xaml.vb
@@ -73,73 +73,63 @@ Class MainWindow
     End Sub
 
     Private Sub BTNLogin_Click(sender As Object, e As RoutedEventArgs) Handles BTNLogin.Click
-        If FLDUsername.Text = "root" And FLDPassword.Password = "root" Then
-            AccountId = "0"
-            UserType = "Administrator"
-            Usrnm = "Super Admin"
-            Me.Hide()
-            Dim mainmenu As New MainMenu
-            mainmenu.Show()
-        Else
-            DB.Open(connectionString)
-            RS.Open("SELECT * FROM tblUsers WHERE Username='" & FLDUsername.Text & Chr(39), DB)
-            Try
-                If (RS.Fields("Username").Value = FLDUsername.Text) And (RS.Fields("Password").Value = FLDPassword.Password) Then
-                    UserType = RS.Fields("LevelofAccess").Value
-                    AccountId = RS.Fields("AccountID").Value
-                    Usrnm = RS.Fields("Username").Value
-                    Me.Hide()
-                    Dim mainmenu As New MainMenu
-                    mainmenu.Show()
-                Else
-                    MessageBox.Show("Invalid Credentials")
+        DB.Open(connectionString)
+        RS.Open("SELECT * FROM tblUsers WHERE Username='" & FLDUsername.Text & Chr(39), DB)
+        Try
+            If (RS.Fields("Username").Value = FLDUsername.Text) And (RS.Fields("Password").Value = FLDPassword.Password) Then
+                UserType = RS.Fields("LevelofAccess").Value
+                AccountId = RS.Fields("AccountID").Value
+                Usrnm = RS.Fields("Username").Value
+                Me.Hide()
+                Dim mainmenu As New MainMenu
+                mainmenu.Show()
+            Else
+                MessageBox.Show("Invalid Credentials")
 
-                End If
-            Catch ex As Exception
-                MessageBox.Show("Account not Found")
-            End Try
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Account not Found")
+        End Try
 
-            RS.Close()
-            DB.Close()
+        RS.Close()
+        DB.Close()
 
-            Try
-                Using act As New OleDbConnection(connectionString)
-                    act.Open()
-                    Dim command As New OleDbCommand("insert into Logs ([AccountID], [ADate], [LevelofAccess], [Action])  values ( @AccountId, @dates, @access, @action)", act)
-                    Dim dt As String
-                    dt = Now.ToLongDateString & " / " & Now.ToLongTimeString
-                    With command.Parameters
-                        .AddWithValue("@AccountId", AccountId.ToUpper)
-                        .AddWithValue("@dates", dt.ToString)
-                        .AddWithValue("@access", UserType.ToString)
-                        .AddWithValue("@action", "LOGGED IN")
-                    End With
-                    command.ExecuteNonQuery()
-                    command.Dispose()
-                    act.Close()
-                End Using
-            Catch ex As Exception
-            End Try
-            Try
-                Using act2 As New OleDbConnection(connectionString)
-                    act2.Open()
-                    Dim command As New OleDbCommand("insert into STATUS ([AccountID],  [LevelofAccess], [Loggedin], [Username])  values ( @AccountId,  @access, @action, @user)", act2)
-                    Dim dt As String
-                    dt = Now.ToLongDateString & " / " & Now.ToLongTimeString
-                    With command.Parameters
-                        .AddWithValue("@AccountId", AccountId.ToUpper)
-                        .AddWithValue("@access", UserType.ToString)
-                        .AddWithValue("@action", "TRUE")
-                        .AddWithValue("@user", Usrnm.ToString)
-                    End With
-                    command.ExecuteNonQuery()
-                    command.Dispose()
-                    act2.Close()
-                End Using
-            Catch ex As Exception
-            End Try
-        End If
-
+        Try
+            Using act As New OleDbConnection(connectionString)
+                act.Open()
+                Dim command As New OleDbCommand("insert into Logs ([AccountID], [ADate], [LevelofAccess], [Action])  values ( @AccountId, @dates, @access, @action)", act)
+                Dim dt As String
+                dt = Now.ToLongDateString & " / " & Now.ToLongTimeString
+                With command.Parameters
+                    .AddWithValue("@AccountId", AccountId.ToUpper)
+                    .AddWithValue("@dates", dt.ToString)
+                    .AddWithValue("@access", UserType.ToString)
+                    .AddWithValue("@action", "LOGGED IN")
+                End With
+                command.ExecuteNonQuery()
+                command.Dispose()
+                act.Close()
+            End Using
+        Catch ex As Exception
+        End Try
+        Try
+            Using act2 As New OleDbConnection(connectionString)
+                act2.Open()
+                Dim command As New OleDbCommand("insert into STATUS ([AccountID],  [LevelofAccess], [Loggedin], [Username])  values ( @AccountId,  @access, @action, @user)", act2)
+                Dim dt As String
+                dt = Now.ToLongDateString & " / " & Now.ToLongTimeString
+                With command.Parameters
+                    .AddWithValue("@AccountId", AccountId.ToUpper)
+                    .AddWithValue("@access", UserType.ToString)
+                    .AddWithValue("@action", "TRUE")
+                    .AddWithValue("@user", Usrnm.ToString)
+                End With
+                command.ExecuteNonQuery()
+                command.Dispose()
+                act2.Close()
+            End Using
+        Catch ex As Exception
+        End Try
     End Sub
 
     Private Sub BTNClear_Click(sender As Object, e As RoutedEventArgs) Handles BTNClear.Click
@@ -155,6 +145,13 @@ Class MainWindow
         CloseProgram(Me)
     End Sub
 
+    Private Sub FLDPassword_KeyDown(sender As Object, e As KeyEventArgs) Handles FLDPassword.KeyDown
+        If (e.Key = Key.Enter) Then
+            BTNLogin.IsDefault = True
+        End If
+    End Sub
+
+
 
     Private Sub MainWindow_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
         If (e.Key = Key.Escape) Then
@@ -169,40 +166,5 @@ Class MainWindow
                 Me.Close()
             End If
         End If
-    End Sub
-
-    Private Sub FLDUsername_PreviewKeyDown(sender As Object, e As KeyEventArgs) Handles FLDUsername.PreviewKeyDown
-        Dim x As String
-        x = e.Key
-        If x = Key.Back Or x = Key.Left Or x = Key.Right Then
-            e.Handled = False
-        ElseIf (Keyboard.IsKeyUp(Key.RightShift) And Keyboard.IsKeyUp(Key.LeftShift) And x >= Key.NumPad0 And x <= Key.NumPad9) Or (Keyboard.IsKeyUp(Key.RightShift) And Keyboard.IsKeyUp(Key.LeftShift) And x >= Key.D0 And x <= Key.D9) Or (x >= Key.A And x <= Key.Z) Or (Keyboard.IsKeyUp(Key.RightShift) And Keyboard.IsKeyUp(Key.LeftShift) And x = Key.OemMinus) Then
-            e.Handled = False
-        ElseIf x = Key.Tab Then
-            FLDUsername.Focus()
-        ElseIf x = Key.Enter Or x = Key.Down Then
-            FLDPassword.Focus()
-        Else
-            e.Handled = True
-        End If
-    End Sub
-
-    Private Sub FLDPassword_PreviewKeyDown(sender As Object, e As KeyEventArgs) Handles FLDPassword.PreviewKeyDown
-        Dim x As String
-        x = e.Key
-        If x = Key.Back Or x = Key.Left Or x = Key.Right Then
-            e.Handled = False
-        ElseIf (Keyboard.IsKeyUp(Key.RightShift) And Keyboard.IsKeyUp(Key.LeftShift) And x >= Key.NumPad0 And x <= Key.NumPad9) Or (Keyboard.IsKeyUp(Key.RightShift) And Keyboard.IsKeyUp(Key.LeftShift) And x >= Key.D0 And x <= Key.D9) Or (x >= Key.A And x <= Key.Z) Then
-            e.Handled = False
-        ElseIf x = Key.Tab Then
-            FLDPassword.Focus()
-        ElseIf x = Key.Enter Then
-            BTNLogin.Focus()
-        ElseIf x = Key.Up Then
-            FLDUsername.Focus()
-        Else
-            e.Handled = True
-        End If
-
     End Sub
 End Class
