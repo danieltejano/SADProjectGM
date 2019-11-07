880c755d4b5b27dbdfcdcad19416943483154aa5
User <User@PC2018051215MZP>
2019-11-07 15:26:08 +0800

    new and old sales

--- SystemAnalysisAndDesignProj/View/SalesPage.xaml.vb
+++ SystemAnalysisAndDesignProj/View/SalesPage.xaml.vb
@@ -10,7 +10,13 @@ Public Class SalesPage
     Public dm As String
 
     Private Sub Sales_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
-        PullDataFromDatabase(d:=GRDSales, tableName:="Sales ORDER BY Date DESC")
+        Dim oleDatabaseConnection As New OleDb.OleDbConnection(connectionString)
+        oleDatabaseConnection.Open()
+        Dim databasez As New OleDbCommand
+        databasez.CommandText = "select * from ProductsPurchased"
+        databasez.Connection = oleDatabaseConnection
+        Dim databaseActualTable As OleDbDataReader = databasez.ExecuteReader()
+        GRDSales.ItemsSource = databaseActualTable
     End Sub
 
     Private Sub GRDSales_AutoGeneratingColumn(sender As Object, e As DataGridAutoGeneratingColumnEventArgs) Handles GRDSales.AutoGeneratingColumn
@@ -18,6 +24,28 @@ Public Class SalesPage
             Dim column As DataGridTextColumn = e.Column
             Dim binding As Binding = column.Binding
             binding.StringFormat = "MMMM-dd-yyyy"
+        End If
+    End Sub
+
+    Private Sub BTN_Click(sender As Object, e As RoutedEventArgs) Handles BTN.Click
+        If BTN.Content = "New Sales" Then
+            Dim oleDatabaseConnection As New OleDb.OleDbConnection(connectionString)
+            oleDatabaseConnection.Open()
+            Dim databasez As New OleDbCommand
+            databasez.CommandText = "select * from ProductsPurchased"
+            databasez.Connection = oleDatabaseConnection
+            Dim databaseActualTable As OleDbDataReader = databasez.ExecuteReader()
+            GRDSales.ItemsSource = databaseActualTable
+            BTN.Content = "Old Sales"
+        ElseIf BTN.Content = "Old Sales" Then
+            Dim oleDatabaseConnection As New OleDb.OleDbConnection(connectionString)
+            oleDatabaseConnection.Open()
+            Dim databasez As New OleDbCommand
+            databasez.CommandText = "select * from Sales"
+            databasez.Connection = oleDatabaseConnection
+            Dim databaseActualTable As OleDbDataReader = databasez.ExecuteReader()
+            GRDSales.ItemsSource = databaseActualTable
+            BTN.Content = "New Sales"
         End If
     End Sub
 End Class
